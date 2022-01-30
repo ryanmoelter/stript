48871cbc5f4db3f111fc7bed27436dcfa11cc468
Ryan Moelter <hello@ryanmoelter.com>
2022-01-30 02:06:25 -0800

    Add interpolation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,44 @@
 import com.diozero.api.SpiDevice
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlin.math.roundToInt
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
 
 const val NUM_LIGHTS = 93
+const val FRAMES_PER_SECOND = 120
+const val FRAME_DELAY_MILLIS = 1000 / FRAMES_PER_SECOND.toLong()
 
-fun main(args: Array<String>) {
+fun main() {
   println("Hello World!")
+
+  val interpolator = FastOutSlowInInterpolator()
 
   // Try adding program arguments via Run/Debug configuration.
   // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
   println("Writing bytes...")
   runBlocking {
     SpiDevice(0).use { spiDevice ->
-      val lastFrame = 119
+      val duration = 4.toDuration(DurationUnit.SECONDS)
+      val lastFrame = (FRAMES_PER_SECOND * duration.inWholeMilliseconds / 1000).toInt()
       for (frameNum in 0..lastFrame) {
         val colors = buildList {
-          (0..NUM_LIGHTS).forEach { lightIndex ->
-            add(
-              if (lightIndex % 10 == 0) {
-                LedColor(0b11111111 * frameNum / lastFrame, 0x0F, 0x00)
-              } else {
-                LedColor(0, 0, 0)
-              }
-            )
+          (0 until NUM_LIGHTS).forEach { lightIndex ->
+            val linearProgress = frameNum.toFloat() / lastFrame
+            val interpolatedProgress = interpolator.getScaledProgressValue(linearProgress)
+            val lightRelativeCoordinateStart = lightIndex.toFloat() / NUM_LIGHTS
+            val lightRelativeCoordinateSize = 1f / NUM_LIGHTS
+
+            val progressModifier =
+              ((interpolatedProgress - lightRelativeCoordinateStart) / lightRelativeCoordinateSize)
+                .coerceIn(0f, 1f)
+
+            val brightness = (0x5F * linearProgress * progressModifier).roundToInt()
+            add(LedColor(red = brightness, green = brightness * 7 / 10, blue = brightness * 5 / 10))
           }
         }
         spiDevice.writeLights(colors)
-        delay(16)
+        delay(FRAME_DELAY_MILLIS)
       }
     }
   }
@@ -34,7 +46,11 @@ fun main(args: Array<String>) {
 }
 
 data class LedColor(val red: UByte, val green: UByte, val blue: UByte) {
-  constructor(red: Int, green: Int, blue: Int) : this(red.toUByte(), green.toUByte(), blue.toUByte())
+  constructor(red: Int, green: Int, blue: Int) : this(
+    red.toUByte(),
+    green.toUByte(),
+    blue.toUByte()
+  )
 }
 
 fun SpiDevice.writeLights(colors: List<LedColor>) {
@@ -45,10 +61,9 @@ fun SpiDevice.writeLights(colors: List<LedColor>) {
   colors.forEach { color ->
     writeColor(color)
   }
-  write(0, 0, 0, 0, 0)
-
+  write(0, 0, 0, 0, 0, 0)
 }
 
 fun SpiDevice.writeColor(color: LedColor) {
-  write(Byte.MAX_VALUE, color.blue.toByte(), color.green.toByte(), color.red.toByte())
+  write(UByte.MAX_VALUE.toByte(), color.blue.toByte(), color.green.toByte(), color.red.toByte())
 }
